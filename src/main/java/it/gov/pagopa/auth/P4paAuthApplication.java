799748979ac1ad8a01e1180940e784f288273ef2
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-04-22 15:38:33 +0200

    first commit

---
+++ src/main/java/it/gov/pagopa/auth/P4paAuthApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P4paAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P4paAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gov/pagopa/auth/P4paAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P4paAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
