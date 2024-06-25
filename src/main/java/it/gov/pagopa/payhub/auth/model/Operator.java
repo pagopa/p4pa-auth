9810037fae3a5696f95db87a0fb9316f6a50a90a
antonio.torre <antonio.torre@nttdata.com>
2024-06-25 15:34:09 +0200

    setting collection name as collections

--- src/main/java/it/gov/pagopa/payhub/auth/model/Operator.java
+++ src/main/java/it/gov/pagopa/payhub/auth/model/Operator.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 import java.util.Set;
 
 @Data
-@Document
+@Document("operators")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
--- src/main/java/it/gov/pagopa/payhub/auth/model/User.java
+++ src/main/java/it/gov/pagopa/payhub/auth/model/User.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 import java.time.LocalDateTime;
 
 @Data
-@Document
+@Document("users")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
