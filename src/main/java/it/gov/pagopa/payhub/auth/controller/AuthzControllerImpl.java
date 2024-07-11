16d3ea1c2c2d49c013111431cd0a432ba19a29a3
antoniocalo <antonio.calo@nttdata.com>
2024-07-11 16:42:35 +0200

    P4ADEV-527 - expose deleteOrganizationOperator API

--- src/main/java/it/gov/pagopa/payhub/auth/controller/AuthzControllerImpl.java
+++ src/main/java/it/gov/pagopa/payhub/auth/controller/AuthzControllerImpl.java
@@ -1,11 +1,17 @@
 package it.gov.pagopa.payhub.auth.controller;
 
+import it.gov.pagopa.payhub.auth.exception.custom.UserNotFoundException;
 import it.gov.pagopa.payhub.auth.exception.custom.UserUnauthorizedException;
+import it.gov.pagopa.payhub.auth.model.Operator;
+import it.gov.pagopa.payhub.auth.model.User;
+import it.gov.pagopa.payhub.auth.repository.OperatorsRepository;
+import it.gov.pagopa.payhub.auth.repository.UsersRepository;
 import it.gov.pagopa.payhub.auth.service.AuthzService;
 import it.gov.pagopa.payhub.auth.utils.SecurityUtils;
 import it.gov.pagopa.payhub.controller.generated.AuthzApi;
 import it.gov.pagopa.payhub.model.generated.OperatorDTO;
 import it.gov.pagopa.payhub.model.generated.OperatorsPage;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthzControllerImpl implements AuthzApi {
 
     private final AuthzService authzService;
-
-    public AuthzControllerImpl(AuthzService authzService) {
+    private final OperatorsRepository operatorsRepository;
+    private final UsersRepository userRepository;
+    public AuthzControllerImpl(AuthzService authzService, OperatorsRepository operatorsRepository, UsersRepository usersRepository) {
         this.authzService = authzService;
+        this.operatorsRepository = operatorsRepository;
+        this.userRepository = usersRepository;
     }
 
     @Override
@@ -33,5 +42,21 @@ public class AuthzControllerImpl implements AuthzApi {
                 .totalElements(organizationOperators.getNumberOfElements())
                 .totalPages(organizationOperators.getTotalPages())
                 .build());
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteOrganizationOperator(String organizationIpaCode, String operatorId) {
+        if(!SecurityUtils.isPrincipalAdmin(organizationIpaCode)){
+            throw new UserUnauthorizedException("User not allowed to delete operator " + operatorId);
+        }
+        Optional<Operator> operator = operatorsRepository.findByOperatorIdAndOrganizationIpaCode(operatorId, organizationIpaCode);
+        operator.ifPresent(operatorFound -> {
+            Optional<User> user = userRepository.findByUserId(operatorFound.getUserId());
+            if(user.isPresent())
+                throw new UserNotFoundException("Can't delete operator with operatorId "+operatorId+" and organizationIpaCode "+organizationIpaCode+" because exists linked user");
+            operatorsRepository.delete(operatorFound);
+        });
+
+        return ResponseEntity.ok(null);
     }
 }
--- src/main/java/it/gov/pagopa/payhub/auth/repository/OperatorsRepository.java
+++ src/main/java/it/gov/pagopa/payhub/auth/repository/OperatorsRepository.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.payhub.auth.repository;
 
 import it.gov.pagopa.payhub.auth.model.Operator;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,5 +10,6 @@ import java.util.List;
 
 public interface OperatorsRepository extends OperatorsRepositoryExt, MongoRepository<Operator, String> {
     List<Operator> findAllByUserId(String userId);
+    Optional<Operator> findByOperatorIdAndOrganizationIpaCode(String OperatorId, String organizationIpaCode);
     Page<Operator> findAllByOrganizationIpaCode(String organizationIpaCode, Pageable pageable);
 }
