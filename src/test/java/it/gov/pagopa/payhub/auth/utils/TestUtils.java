7bdc7118a358a407c59db6cd2f24a1deefdbb78b
macacia <marco.cacia@eng.it>
2024-10-22 15:23:51 +0200

    feat: P4ADEV-1276-create-access-token (#98)

---
+++ src/test/java/it/gov/pagopa/payhub/auth/utils/TestUtils.java
@@ -0,0 +1,25 @@
+package it.gov.pagopa.payhub.auth.utils;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestUtils {
+    private TestUtils(){}
+
+    /**
+     * It will assert not null on all o's fields
+     */
+    public static void checkNotNullFields(Object o, String... excludedFields) {
+        Set<String> excludedFieldsSet = new HashSet<>(Arrays.asList(excludedFields));
+        org.springframework.util.ReflectionUtils.doWithFields(o.getClass(),
+                f -> {
+                    f.setAccessible(true);
+                    Assertions.assertNotNull(f.get(o), "The field "+f.getName()+" of the input object of type "+o.getClass()+" is null!");
+                },
+                f -> !excludedFieldsSet.contains(f.getName()));
+    }
+
+}
