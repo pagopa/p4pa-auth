ec2dfc9e98c83eb6b03d22138625237c764aa87a
macacia <marco.cacia@eng.it>
2024-10-22 15:23:51 +0200

    feat: P4ADEV-1276-create-access-token (#98)

--- src/main/java/it/gov/pagopa/payhub/auth/service/a2a/ClientCredentialServiceImpl.java
+++ src/main/java/it/gov/pagopa/payhub/auth/service/a2a/ClientCredentialServiceImpl.java
@@ -1,6 +1,11 @@
 package it.gov.pagopa.payhub.auth.service.a2a;
 
+import it.gov.pagopa.payhub.auth.dto.IamUserInfoDTO;
+import it.gov.pagopa.payhub.auth.mapper.ClientDTO2UserInfoMapper;
+import it.gov.pagopa.payhub.auth.service.AccessTokenBuilderService;
+import it.gov.pagopa.payhub.auth.service.TokenStoreService;
 import it.gov.pagopa.payhub.model.generated.AccessToken;
+import it.gov.pagopa.payhub.model.generated.ClientDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +15,30 @@ public class ClientCredentialServiceImpl implements ClientCredentialService {
 
 	private final ValidateClientCredentialsService validateClientCredentialsService;
 	private final AuthorizeClientCredentialsRequestService authorizeClientCredentialsRequestService;
+	private final AccessTokenBuilderService accessTokenBuilderService;
+	private final TokenStoreService tokenStoreService;
+	private final ClientDTO2UserInfoMapper clientDTO2UserInfoMapper;
 
-	public ClientCredentialServiceImpl(ValidateClientCredentialsService validateClientCredentialsService, AuthorizeClientCredentialsRequestService authorizeClientCredentialsRequestService) {
+	public ClientCredentialServiceImpl(
+			ValidateClientCredentialsService validateClientCredentialsService,
+			AuthorizeClientCredentialsRequestService authorizeClientCredentialsRequestService,
+			AccessTokenBuilderService accessTokenBuilderService,
+			TokenStoreService tokenStoreService, ClientDTO2UserInfoMapper clientDTO2UserInfoMapper) {
 		this.validateClientCredentialsService = validateClientCredentialsService;
 		this.authorizeClientCredentialsRequestService = authorizeClientCredentialsRequestService;
+		this.accessTokenBuilderService = accessTokenBuilderService;
+		this.tokenStoreService = tokenStoreService;
+		this.clientDTO2UserInfoMapper = clientDTO2UserInfoMapper;
 	}
 
 	@Override
 	public AccessToken postToken(String clientId, String scope, String clientSecret) {
 		log.info("Client {} requested authentication with client_credentials grant type and scope {}", clientId, scope);
 		validateClientCredentialsService.validate(scope, clientSecret);
-		authorizeClientCredentialsRequestService.authorizeCredentials(clientId, clientSecret);
-		return AccessToken.builder().accessToken("accessToken").build();
+		ClientDTO authorizedClient = authorizeClientCredentialsRequestService.authorizeCredentials(clientId, clientSecret);
+		AccessToken accessToken = accessTokenBuilderService.build();
+		IamUserInfoDTO iamUser = clientDTO2UserInfoMapper.apply(authorizedClient);
+		tokenStoreService.save(accessToken.getAccessToken(), iamUser);
+		return accessToken;
 	}
 }
--- src/test/java/it/gov/pagopa/payhub/auth/service/a2a/ClientCredentialsServiceTest.java
+++ src/test/java/it/gov/pagopa/payhub/auth/service/a2a/ClientCredentialsServiceTest.java
@@ -1,5 +1,9 @@
 package it.gov.pagopa.payhub.auth.service.a2a;
 
+import it.gov.pagopa.payhub.auth.dto.IamUserInfoDTO;
+import it.gov.pagopa.payhub.auth.mapper.ClientDTO2UserInfoMapper;
+import it.gov.pagopa.payhub.auth.service.AccessTokenBuilderService;
+import it.gov.pagopa.payhub.auth.service.TokenStoreService;
 import it.gov.pagopa.payhub.model.generated.AccessToken;
 import it.gov.pagopa.payhub.model.generated.ClientDTO;
 import org.junit.jupiter.api.Assertions;
@@ -17,12 +21,24 @@ class ClientCredentialsServiceTest {
 	private ValidateClientCredentialsService validateClientCredentialsServiceMock;
 	@Mock
 	private AuthorizeClientCredentialsRequestService authorizeClientCredentialsRequestServiceMock;
+	@Mock
+	private AccessTokenBuilderService accessTokenBuilderServiceMock;
+	@Mock
+	private TokenStoreService tokenStoreServiceMock;
+	@Mock
+	private ClientDTO2UserInfoMapper clientDTO2UserInfoMapperMock;
 
 	private ClientCredentialService service;
 
 	@BeforeEach
 	void init() {
-		service = new ClientCredentialServiceImpl(validateClientCredentialsServiceMock, authorizeClientCredentialsRequestServiceMock);
+		service = new ClientCredentialServiceImpl(
+			validateClientCredentialsServiceMock,
+			authorizeClientCredentialsRequestServiceMock,
+			accessTokenBuilderServiceMock,
+			tokenStoreServiceMock,
+			clientDTO2UserInfoMapperMock
+		);
 	}
 
 	@Test
@@ -33,12 +49,17 @@ class ClientCredentialsServiceTest {
 		String clientSecret="CLIENT_SECRET";
 
 		Mockito.doNothing().when(validateClientCredentialsServiceMock).validate(scope, clientSecret);
-		Mockito.doReturn(new ClientDTO()).when(authorizeClientCredentialsRequestServiceMock).authorizeCredentials(clientId, clientSecret);
+		ClientDTO clientDTO = new ClientDTO();
+		Mockito.doReturn(clientDTO).when(authorizeClientCredentialsRequestServiceMock).authorizeCredentials(clientId, clientSecret);
 		AccessToken expectedAccessToken = AccessToken.builder().accessToken("accessToken").build();
+		Mockito.when(accessTokenBuilderServiceMock.build()).thenReturn(expectedAccessToken);
+		IamUserInfoDTO iamUserInfo = new IamUserInfoDTO();
+		Mockito.when(clientDTO2UserInfoMapperMock.apply(clientDTO)).thenReturn(iamUserInfo);
 		//When
 		AccessToken result = service.postToken(clientId, scope, clientSecret);
 		//Then
-		Assertions.assertEquals(expectedAccessToken, result);
+		Assertions.assertSame(expectedAccessToken, result);
+		Mockito.verify(tokenStoreServiceMock).save(Mockito.same(expectedAccessToken.getAccessToken()), Mockito.same(iamUserInfo));
 	}
 
 }
