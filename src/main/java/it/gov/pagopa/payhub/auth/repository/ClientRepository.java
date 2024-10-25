abf6def753d131399f03ebb992db76dddc8328b1
macacia <marco.cacia@eng.it>
2024-10-25 14:28:48 +0200

    feat: P4ADEV-1102-create-client-revoke-API (#103)

--- src/main/java/it/gov/pagopa/payhub/auth/repository/ClientRepository.java
+++ src/main/java/it/gov/pagopa/payhub/auth/repository/ClientRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface ClientRepository extends MongoRepository<Client, String> {
-
 	List<Client> findAllByOrganizationIpaCode(String organizationIpaCode);
+	long deleteByClientIdAndOrganizationIpaCode(String clientId, String organizationIpaCode);
 }
