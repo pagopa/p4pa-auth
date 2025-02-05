4f48418b10625ac091a91b70de22272d6644e15d
antonio.torre <antonio.torre@pagopa.it>
2025-02-05 12:21:28 +0100

    fix ErrorHandling and LocalDateTime json serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ val micrometerVersion = "1.4.1"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
 	implementation("io.micrometer:micrometer-registry-prometheus")
