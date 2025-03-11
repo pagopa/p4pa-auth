541a2ee3cb03a44c27246adcd27f5870c788b398
antonio.torre <antonio.torre@pagopa.it>
2025-03-11 18:50:47 +0100

    feat: Configuring build-info on actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
-	jacoco
-	id("org.sonarqube") version "6.0.1.5171"
-	id("com.github.ben-manes.versions") version "0.51.0"
-	id("org.openapi.generator") version "7.10.0"
-	id("org.ajoberstar.grgit") version "5.3.0"
+    java
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    jacoco
+    id("org.sonarqube") version "6.0.1.5171"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.openapi.generator") version "7.10.0"
+    id("org.ajoberstar.grgit") version "5.3.0"
+    id("com.gorylenko.gradle-git-properties") version "2.5.0"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -14,20 +15,20 @@ version = "0.1.0"
 description = "p4pa-auth"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 val springDocOpenApiVersion = "2.8.5"
@@ -43,157 +44,158 @@ val micrometerVersion = "1.4.3"
 val caffeineVersion = "3.2.0"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
-	implementation("io.micrometer:micrometer-registry-prometheus")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.springframework.boot:spring-boot-starter-cache")
-	implementation("com.github.ben-manes.caffeine:caffeine:$caffeineVersion")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
-	implementation("org.codehaus.janino:janino:$janinoVersion")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-	implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("com.github.ben-manes.caffeine:caffeine:$caffeineVersion")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
+    implementation("org.codehaus.janino:janino:$janinoVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
 
-	// validation token jwt
-	implementation("com.auth0:java-jwt:$javaJwtVersion")
-	implementation("com.auth0:jwks-rsa:$jwksRsaVersion")
-	implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")
-	implementation("io.jsonwebtoken:jjwt:$jjwtVersion")
-	implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
+    // validation token jwt
+    implementation("com.auth0:java-jwt:$javaJwtVersion")
+    implementation("com.auth0:jwks-rsa:$jwksRsaVersion")
+    implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")
+    implementation("io.jsonwebtoken:jjwt:$jjwtVersion")
+    implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
 
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testAnnotationProcessor("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
 
-	//	Testing
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("org.junit.jupiter:junit-jupiter-api")
-	testImplementation("org.junit.jupiter:junit-jupiter-engine")
-	testImplementation("org.mockito:mockito-core")
-	testImplementation ("org.projectlombok:lombok")
-	testImplementation ("org.wiremock:wiremock-standalone:$wiremockVersion")
+    //	Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.mockito:mockito-core")
+    testImplementation("org.projectlombok:lombok")
+    testImplementation("org.wiremock:wiremock-standalone:$wiremockVersion")
 
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	finalizedBy(tasks.jacocoTestReport)
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 val mockitoAgent = configurations.create("mockitoAgent")
 dependencies {
-	mockitoAgent("org.mockito:mockito-core") { isTransitive = false }
+    mockitoAgent("org.mockito:mockito-core") { isTransitive = false }
 }
 tasks {
-	test {
-		jvmArgs("-javaagent:${mockitoAgent.asPath}")
-	}
+    test {
+        jvmArgs("-javaagent:${mockitoAgent.asPath}")
+    }
 }
 
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
-	reports {
-		xml.required = true
-	}
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
 }
 
 val projectInfo = mapOf(
-		"artifactId" to project.name,
-		"version" to project.version
+    "artifactId" to project.name,
+    "version" to project.version
 )
 
 tasks {
-	val processResources by getting(ProcessResources::class) {
-		filesMatching("**/application.yml") {
-			expand(projectInfo)
-		}
-	}
+    val processResources by getting(ProcessResources::class) {
+        filesMatching("**/application.yml") {
+            expand(projectInfo)
+        }
+    }
 }
 
 configurations {
-	compileClasspath {
-		resolutionStrategy.activateDependencyLocking()
-	}
+    compileClasspath {
+        resolutionStrategy.activateDependencyLocking()
+    }
 }
 
 tasks.compileJava {
-	dependsOn("dependenciesBuild")
+    dependsOn("dependenciesBuild")
 }
 
 tasks.register("dependenciesBuild") {
-	group = "AutomaticallyGeneratedCode"
-	description = "grouping all together automatically generate code tasks"
+    group = "AutomaticallyGeneratedCode"
+    description = "grouping all together automatically generate code tasks"
 
-	dependsOn(
-		"openApiGenerateP4PAAUTH",
-		"openApiGenerateORGANIZATION"
-	)
+    dependsOn(
+        "openApiGenerateP4PAAUTH",
+        "openApiGenerateORGANIZATION"
+    )
 }
 
 configure<SourceSetContainer> {
-	named("main") {
-		java.srcDir("$projectDir/build/generated/src/main/java")
-	}
+    named("main") {
+        java.srcDir("$projectDir/build/generated/src/main/java")
+    }
 }
 
 springBoot {
-	mainClass.value("it.gov.pagopa.payhub.auth.PayhubAuthApplication")
+    buildInfo()
+    mainClass.value("it.gov.pagopa.payhub.auth.PayhubAuthApplication")
 }
 
 tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("openApiGenerateP4PAAUTH") {
-	group = "openapi"
-	description = "description"
+    group = "openapi"
+    description = "description"
 
-	generatorName.set("spring")
-	inputSpec.set("$rootDir/openapi/p4pa-auth.openapi.yaml")
-	outputDir.set("$projectDir/build/generated")
-	apiPackage.set("it.gov.pagopa.payhub.controller.generated")
-	modelPackage.set("it.gov.pagopa.payhub.dto.generated")
-	configOptions.set(mapOf(
-		"dateLibrary" to "java8",
-		"requestMappingMode" to "api_interface",
-		"useSpringBoot3" to "true",
-		"interfaceOnly" to "true",
-		"useTags" to "true",
-		"useBeanValidation" to "true",
-		"generateConstructorWithAllArgs" to "true",
-		"generatedConstructorWithRequiredArgs" to "true",
-		"additionalModelTypeAnnotations" to "@lombok.experimental.SuperBuilder(toBuilder = true)"
-	))
+    generatorName.set("spring")
+    inputSpec.set("$rootDir/openapi/p4pa-auth.openapi.yaml")
+    outputDir.set("$projectDir/build/generated")
+    apiPackage.set("it.gov.pagopa.payhub.controller.generated")
+    modelPackage.set("it.gov.pagopa.payhub.dto.generated")
+    configOptions.set(mapOf(
+        "dateLibrary" to "java8",
+        "requestMappingMode" to "api_interface",
+        "useSpringBoot3" to "true",
+        "interfaceOnly" to "true",
+        "useTags" to "true",
+        "useBeanValidation" to "true",
+        "generateConstructorWithAllArgs" to "true",
+        "generatedConstructorWithRequiredArgs" to "true",
+        "additionalModelTypeAnnotations" to "@lombok.experimental.SuperBuilder(toBuilder = true)"
+    ))
 }
 
 var targetEnv = when (grgit.branch.current().name) {
-	"uat" -> "uat"
-	"main" -> "main"
-	else -> "develop"
+    "uat" -> "uat"
+    "main" -> "main"
+    else -> "develop"
 }
 
 tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("openApiGenerateORGANIZATION") {
-	group = "openapi"
-	description = "description"
+    group = "openapi"
+    description = "description"
 
-	generatorName.set("java")
-	remoteInputSpec.set("https://raw.githubusercontent.com/pagopa/p4pa-organization/refs/heads/$targetEnv/openapi/generated.openapi.json")
-	outputDir.set("$projectDir/build/generated")
-	apiPackage.set("it.gov.pagopa.pu.p4pa-organization.controller.generated")
-	modelPackage.set("it.gov.pagopa.pu.p4pa-organization.dto.generated")
-	configOptions.set(mapOf(
-		"swaggerAnnotations" to "false",
-		"openApiNullable" to "false",
-		"dateLibrary" to "java17",
-		"useSpringBoot3" to "true",
-		"useJakartaEe" to "true",
-		"serializationLibrary" to "jackson",
-		"generateSupportingFiles" to "true",
-		"generateConstructorWithAllArgs" to "true",
-		"generatedConstructorWithRequiredArgs" to "true",
-		"additionalModelTypeAnnotations" to "@lombok.experimental.SuperBuilder(toBuilder = true)"
-	))
-	library.set("resttemplate")
+    generatorName.set("java")
+    remoteInputSpec.set("https://raw.githubusercontent.com/pagopa/p4pa-organization/refs/heads/$targetEnv/openapi/generated.openapi.json")
+    outputDir.set("$projectDir/build/generated")
+    apiPackage.set("it.gov.pagopa.pu.p4pa-organization.controller.generated")
+    modelPackage.set("it.gov.pagopa.pu.p4pa-organization.dto.generated")
+    configOptions.set(mapOf(
+        "swaggerAnnotations" to "false",
+        "openApiNullable" to "false",
+        "dateLibrary" to "java17",
+        "useSpringBoot3" to "true",
+        "useJakartaEe" to "true",
+        "serializationLibrary" to "jackson",
+        "generateSupportingFiles" to "true",
+        "generateConstructorWithAllArgs" to "true",
+        "generatedConstructorWithRequiredArgs" to "true",
+        "additionalModelTypeAnnotations" to "@lombok.experimental.SuperBuilder(toBuilder = true)"
+    ))
+    library.set("resttemplate")
 }
