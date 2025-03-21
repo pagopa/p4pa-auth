0789058b605cf845bf08dafb35ad4a8f3971bb8a
antonio.torre <antonio.torre@pagopa.it>
2025-03-21 18:08:46 +0100

    fix: Exposing enum as ref

--- src/main/java/it/gov/pagopa/payhub/auth/config/SwaggerConfig.java
+++ src/main/java/it/gov/pagopa/payhub/auth/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -24,5 +26,9 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT",
         scheme = "bearer"
 )
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SwaggerConfig {
+    static {
+        io.swagger.v3.core.jackson.ModelResolver.enumsAsRef = true;
+    }
 }
