2ba77f73ddeafc191052dc9bc8f23af0a275829b
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-08-25 15:19:32 +0200

    P4ADEV-3687 fix CVE-2025-48989

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.util.Objects
 
 plugins {
     java
-    id("org.springframework.boot") version "3.5.3"
+    id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
     jacoco
     id("org.sonarqube") version "6.2.0.5505"
