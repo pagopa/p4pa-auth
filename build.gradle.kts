5715346bae7e773786bdbe7ec6f4503fb443ff84
antonio.torre <antonio.torre@pagopa.it>
2025-09-18 17:37:12 +0200

    fix: P4ADEV-3829 CVE fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import java.util.Objects
 
 plugins {
     java
-    id("org.springframework.boot") version "3.5.5"
+    id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
     jacoco
-    id("org.sonarqube") version "6.2.0.5505"
+    id("org.sonarqube") version "6.3.1.5724"
     id("com.github.ben-manes.versions") version "0.52.0"
-    id("org.openapi.generator") version "7.13.0"
+    id("org.openapi.generator") version "7.15.0"
     id("org.ajoberstar.grgit") version "5.3.2"
-    id("com.gorylenko.gradle-git-properties") version "2.5.0"
+    id("com.gorylenko.gradle-git-properties") version "2.5.3"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -33,17 +33,17 @@ repositories {
     mavenCentral()
 }
 
-val springDocOpenApiVersion = "2.8.9"
+val springDocOpenApiVersion = "2.8.13"
 val janinoVersion = "3.1.12"
-val openApiToolsVersion = "0.2.6"
+val openApiToolsVersion = "0.2.7"
 val javaJwtVersion = "4.5.0"
-val jwksRsaVersion = "0.22.2"
-val nimbusJoseJwtVersion = "10.3"
-val jjwtVersion = "0.12.6"
+val jwksRsaVersion = "0.23.0"
+val nimbusJoseJwtVersion = "10.5"
+val jjwtVersion = "0.13.0"
 val wiremockVersion = "3.13.1"
-val bouncycastleVersion = "1.81"
-val micrometerVersion = "1.5.1"
-val caffeineVersion = "3.2.1"
+val bouncycastleVersion = "1.82"
+val micrometerVersion = "1.5.4"
+val caffeineVersion = "3.2.2"
 val httpClientVersion = "5.5"
 
 dependencies {
