df70d993e94be1ef21e90067cee14853f7419f85
antonio.torre <antonio.torre@pagopa.it>
2025-11-07 13:43:33 +0100

    Added X-Trace-Id response header + Added traceId field on ErrorDTO

---
+++ src/main/java/it/gov/pagopa/payhub/auth/utils/Utilities.java
@@ -0,0 +1,11 @@
+package it.gov.pagopa.payhub.auth.utils;
+
+import org.slf4j.MDC;
+
+public class Utilities {
+    private Utilities(){}
+
+    public static String getTraceId(){
+        return MDC.get("traceId");
+    }
+}
---
+++ src/test/java/it/gov/pagopa/payhub/auth/utils/UtilitiesTest.java
@@ -0,0 +1,30 @@
+package it.gov.pagopa.payhub.auth.utils;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.MDC;
+
+public class UtilitiesTest {
+
+  @Test
+  void testGetTraceId(){
+    // Given
+    String expectedResult = "TRACEID";
+    setTraceId(expectedResult);
+
+    // When
+    String result = Utilities.getTraceId();
+
+    // Then
+    Assertions.assertSame(expectedResult, result);
+    clearTraceIdContext();
+  }
+
+  public static void setTraceId(String traceId) {
+    MDC.put("traceId", traceId);
+  }
+  public static void clearTraceIdContext(){
+    MDC.clear();
+  }
+}
