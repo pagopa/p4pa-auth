ebaf68f7164b6ad49f421ef9b99fea9e74e1ead6
Dan Bota <dan.bota@nttdata.com>
2025-11-11 12:16:21 +0100

    test: removed unusued import

--- src/test/java/it/gov/pagopa/payhub/auth/mapper/Client2UserInfoMapperTest.java
+++ src/test/java/it/gov/pagopa/payhub/auth/mapper/Client2UserInfoMapperTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.MDC;
 
 import java.util.Collections;
 
