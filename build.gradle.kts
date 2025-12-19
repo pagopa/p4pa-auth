a4acc8f42bf4c3cd38d5bcdce2e7b395170048f1
antonio.torre <antonio.torre@pagopa.it>
2025-12-19 12:47:52 +0100

    fix: Fix CVE, upgrade SpringBoot 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,6 @@ dependencies {
     }
     implementation("org.apache.commons:commons-lang3:$commonsLang3Version")
     implementation("org.codehaus.janino:janino:$janinoVersion")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
     implementation("org.apache.httpcomponents.client5:httpclient5:$httpClientVersion")
     implementation("com.github.danielwegener:logback-kafka-appender:$kafkaAppender") {
