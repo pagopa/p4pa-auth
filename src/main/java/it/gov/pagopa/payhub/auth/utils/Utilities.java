7b9d5a63673dddf74bbff4aa2863c51b7844fd7c
macacia <marco.cacia@eng.it>
2026-01-13 16:40:51 +0100

    add conversion method

--- src/main/java/it/gov/pagopa/payhub/auth/utils/Utilities.java
+++ src/main/java/it/gov/pagopa/payhub/auth/utils/Utilities.java
@@ -2,10 +2,20 @@ package it.gov.pagopa.payhub.auth.utils;
 
 import org.slf4j.MDC;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
 public class Utilities {
     private Utilities() {}
 
     public static String getTraceId(){
         return MDC.get("traceId");
+    }
+
+    public static OffsetDateTime localDatetimeToOffsetDateTime(LocalDateTime localDateTime) {
+        return localDateTime != null
+            ? localDateTime.atOffset(ZoneId.systemDefault().getRules().getOffset(localDateTime))
+            : null;
     }
 }
--- src/test/java/it/gov/pagopa/payhub/auth/utils/UtilitiesTest.java
+++ src/test/java/it/gov/pagopa/payhub/auth/utils/UtilitiesTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.MDC;
 
+import java.time.OffsetDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class UtilitiesTest {
 
   @Test
@@ -26,5 +31,19 @@ public class UtilitiesTest {
   }
   public static void clearTraceIdContext(){
     MDC.clear();
+  }
+
+  @Test
+  void testLocalDatetimeToOffsetDateTimeWithNull() {
+    assertNull(Utilities.localDatetimeToOffsetDateTime(null), "The result should be null for a null input.");
+  }
+
+  @Test
+  void testLocalDatetimeToOffsetDateTime() {
+    OffsetDateTime expectedOffsetDateTime = OffsetDateTime.now();
+
+    OffsetDateTime result = Utilities.localDatetimeToOffsetDateTime(expectedOffsetDateTime.toLocalDateTime());
+
+    assertEquals(expectedOffsetDateTime, result);
   }
 }
