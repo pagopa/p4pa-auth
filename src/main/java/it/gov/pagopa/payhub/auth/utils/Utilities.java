008d4599b3d00d183e3aad877bd30a45b48610f9
macacia <marco.cacia@eng.it>
2026-01-14 12:20:42 +0100

    feat: P4ADEV-4340 add attribute lastLogin to OperatorDTO (#206)

--- src/main/java/it/gov/pagopa/payhub/auth/utils/Utilities.java
+++ src/main/java/it/gov/pagopa/payhub/auth/utils/Utilities.java
@@ -2,10 +2,20 @@ package it.gov.pagopa.payhub.auth.utils;
 
 import org.slf4j.MDC;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+
 public class Utilities {
     private Utilities() {}
 
     public static String getTraceId(){
         return MDC.get("traceId");
+    }
+
+    public static OffsetDateTime localDatetimeToOffsetDateTime(LocalDateTime localDateTime) {
+        return localDateTime != null
+            ? ZonedDateTime.of(localDateTime, Constants.ZONEID).toOffsetDateTime()
+            : null;
     }
 }
