0350c3c640fae244d50c924e9b07772ad8b3a87f
antonio.torre <antonio.torre@pagopa.it>
2026-01-22 17:08:52 +0100

    chore: Added AZURE_DEVOPS_TOKEN on GitHub Action secrets through terraform

--- .github/terraform/99_locals.tf
+++ .github/terraform/99_locals.tf
@@ -9,8 +9,9 @@ locals {
   env_variables = {}
 
   repo_secrets = var.env_short == "p" ? {
-    SONAR_TOKEN       = data.azurerm_key_vault_secret.sonar_token[0].value
-    SLACK_WEBHOOK_URL = data.azurerm_key_vault_secret.slack_webhook[0].value
+    SONAR_TOKEN        = data.azurerm_key_vault_secret.sonar_token[0].value
+    SLACK_WEBHOOK_URL  = data.azurerm_key_vault_secret.slack_webhook[0].value
+    AZURE_DEVOPS_TOKEN = data.azurerm_key_vault_secret.azure_devops_token[0].value
   } : {}
 
   repo_env = var.env_short == "p" ? {
