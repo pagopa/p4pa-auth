7bc28a70667898a244daf2ef5d3fe9f57d219ff0
antonio.torre <antonio.torre@pagopa.it>
2026-02-18 00:01:58 +0100

    fix: Introducing tech fields

--- src/main/java/it/gov/pagopa/payhub/auth/utils/SecurityUtils.java
+++ src/main/java/it/gov/pagopa/payhub/auth/utils/SecurityUtils.java
@@ -1,33 +1,72 @@
 package it.gov.pagopa.payhub.auth.utils;
 
+import it.gov.pagopa.payhub.auth.mapper.Client2UserInfoMapper;
 import it.gov.pagopa.payhub.dto.generated.UserInfo;
 import it.gov.pagopa.payhub.dto.generated.UserOrganizationRoles;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+
+import static it.gov.pagopa.payhub.auth.service.m2m.AuthorizeClientCredentialsRequestService.PIATTAFORMA_UNITARIA_CLIENT_ID_PREFIX;
 
 public final class SecurityUtils {
     private SecurityUtils(){}
 
+    public static final String SYSTEM_USERID_PREFIX  = Client2UserInfoMapper.buildSystemMappedExternalUserId(PIATTAFORMA_UNITARIA_CLIENT_ID_PREFIX);
+    public static final String HEADER_USER_ID = "X-user-id";
+
     /** It will return user's session accessToken */
     public static String getAccessToken() {
-        return (String) SecurityContextHolder.getContext().getAuthentication().getCredentials();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication!=null) {
+            return (String) authentication.getCredentials();
+        } else {
+            return null;
+        }
     }
 
     /** It will return user's session data from ThreadLocal */
     public static UserInfo getPrincipal(){
-        return (UserInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication!=null) {
+            return (UserInfo) authentication.getPrincipal();
+        } else {
+            return null;
+        }
     }
 
     /** It will return user's session roles on requested organization IPA code retrieving it from ThreadLocal */
     public static List<String> getPrincipalRoles(String organizationIpaCode){
-        return getPrincipal().getOrganizations().stream()
+        return Objects.requireNonNull(getPrincipal()).getOrganizations().stream()
                 .filter(o -> organizationIpaCode.equals(o.getOrganizationIpaCode()))
                 .findFirst()
                 .map(UserOrganizationRoles::getRoles)
                 .orElse(Collections.emptyList());
+    }
+
+    public static String getCurrentUserExternalId(){
+        UserInfo loggedUser = getPrincipal();
+        if(loggedUser!=null) {
+            return resolvePuSystemUser(loggedUser.getMappedExternalUserId());
+        } else {
+            return null;
+        }
+    }
+
+    public static String resolvePuSystemUser(String mappedExternalUserId) {
+        if(mappedExternalUserId != null && mappedExternalUserId.startsWith(SYSTEM_USERID_PREFIX) && RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes servletRequestAttributes){
+            HttpServletRequest requestAttributes = servletRequestAttributes.getRequest();
+            mappedExternalUserId = ObjectUtils.firstNonNull(requestAttributes.getHeader(HEADER_USER_ID), mappedExternalUserId);
+        }
+        return mappedExternalUserId;
     }
 
     /** It will return true if the user's session has ROLE_ADMIN role on requested organization IPA code retrieving it from ThreadLocal */
@@ -37,9 +76,14 @@ public final class SecurityUtils {
 
     /** It will return true if the user has at least a ROLE_ADMIN on one of organization retrieving it from ThreadLocal */
     public static boolean hasAdminRole() {
-        return getPrincipal().getOrganizations().stream()
-            .flatMap(o -> o.getRoles().stream())
-            .anyMatch(Constants.ROLE_ADMIN::equals);
+        UserInfo principal = getPrincipal();
+        if(principal!=null) {
+            return principal.getOrganizations().stream()
+                    .flatMap(o -> o.getRoles().stream())
+                    .anyMatch(Constants.ROLE_ADMIN::equals);
+        } else {
+            return false;
+        }
     }
 
     public static String removePiiFromURI(URI uri){
--- src/test/java/it/gov/pagopa/payhub/auth/repository/OperatorsRepositoryExtImplTest.java
+++ src/test/java/it/gov/pagopa/payhub/auth/repository/OperatorsRepositoryExtImplTest.java
@@ -18,7 +18,7 @@ import org.springframework.data.mongodb.core.query.Update;
 import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
-class OperatorsRepositoryExtImplTest {
+class OperatorsRepositoryExtImplTest extends BaseMongoRepositoryTest {
 
     @Mock
     private MongoTemplate mongoTemplateMock;
@@ -38,23 +38,26 @@ class OperatorsRepositoryExtImplTest {
     @Test
     void whenRegisterUserThenReturnStoredUser() {
         // Given
-        String userId="USERID";
-        String organizationIpaCode="ORGANIZATIONIPACODE";
+        String userId = "USERID";
+        String organizationIpaCode = "ORGANIZATIONIPACODE";
         String email = "EMAIL";
         Set<String> roles = Set.of("ROLE");
         Operator storedOperator = new Operator();
 
-        Mockito.when(mongoTemplateMock.findAndModify(
-                Mockito.eq(Query.query(Criteria
-                        .where(Operator.Fields.operatorId).is(userId+organizationIpaCode))),
-                Mockito.eq(new Update()
-                        .set(Operator.Fields.userId, userId)
-                        .set(Operator.Fields.organizationIpaCode, organizationIpaCode)
-                        .set(Operator.Fields.email, email)
-                        .set(Operator.Fields.roles, roles)),
-                Mockito.argThat(opt -> opt.isReturnNew() && opt.isUpsert() && !opt.isRemove()),
-                Mockito.eq(Operator.class)
-        )).thenReturn(storedOperator);
+        Mockito.doReturn(storedOperator)
+                .when(mongoTemplateMock)
+                .findAndModify(
+                        Mockito.eq(Query.query(Criteria
+                                .where(Operator.Fields.operatorId).is(userId + organizationIpaCode))),
+                        Mockito.argThat(argSetTechFieldsOnDocumentUpdateInvoke(new Update()
+                                .set(Operator.Fields.userId, userId)
+                                .set(Operator.Fields.organizationIpaCode, organizationIpaCode)
+                                .set(Operator.Fields.email, email)
+                                .set(Operator.Fields.roles, roles)
+                        )),
+                        Mockito.argThat(opt -> opt.isReturnNew() && opt.isUpsert() && !opt.isRemove()),
+                        Mockito.eq(Operator.class)
+                );
 
         // When
         Operator result = repository.registerOperator(userId, organizationIpaCode, email, roles);
@@ -74,17 +77,17 @@ class OperatorsRepositoryExtImplTest {
         user.setUserId(userId);
 
         Mockito.when(mongoTemplateMock.findOne(
-            Query.query(Criteria.where(User.Fields.mappedExternalUserId).is(mappedExternalUserId)),
-            User.class)).thenReturn(user);
+                Query.query(Criteria.where(User.Fields.mappedExternalUserId).is(mappedExternalUserId)),
+                User.class)).thenReturn(user);
 
         // When
         repository.deleteOrganizationOperator(organizationIpaCode, mappedExternalUserId);
 
         // Then
         Mockito.verify(mongoTemplateMock).remove(
-            Query.query(Criteria
-                .where(Operator.Fields.organizationIpaCode).is(organizationIpaCode)
-                .and(Operator.Fields.userId).is(userId)),
-            Operator.class);
+                Query.query(Criteria
+                        .where(Operator.Fields.organizationIpaCode).is(organizationIpaCode)
+                        .and(Operator.Fields.userId).is(userId)),
+                Operator.class);
     }
 }
