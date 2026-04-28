ff462110adc059735f088e0a304a18543e2a9d48
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2026-04-28 18:58:30 +0200

    fix: Fix CVE (#227)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
-    id("org.springframework.boot") version "4.0.5"
+    id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
     jacoco
     id("org.sonarqube") version "7.2.3.7755"
-    id("com.github.ben-manes.versions") version "0.53.0"
+    id("com.github.ben-manes.versions") version "0.54.0"
     id("org.openapi.generator") version "7.21.0"
     id("org.ajoberstar.grgit") version "5.3.2"
     id("com.gorylenko.gradle-git-properties") version "2.5.7"
@@ -49,25 +49,22 @@ repositories {
     mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.2"
+val springDocOpenApiVersion = "3.0.3"
 val janinoVersion = "3.1.12"
 val openApiToolsVersion = "0.2.10"
 val javaJwtVersion = "4.5.1"
-val jwksRsaVersion = "0.23.0"
+val jwksRsaVersion = "0.23.1"
 val nimbusJoseJwtVersion = "10.9"
 val jjwtVersion = "0.13.0"
 val wiremockVersion = "3.13.2"
-val bouncycastleVersion = "1.83"
-val micrometerVersion = "1.6.4"
+val bouncycastleVersion = "1.84"
+val micrometerVersion = "1.6.5"
 val caffeineVersion = "3.2.3"
-val httpClientVersion = "5.6"
+val httpClientVersion = "5.6.1"
 val httpCoreVersion = "5.4.2"
 val kafkaAppender = "0.2.0-RC2"
 val commonsLang3Version = "3.20.0"
-val lz4JavaVersion = "1.10.4"
-
-// fix cve
-val jackson3CoreVersion = "3.1.1"
+val lz4JavaVersion = "1.11.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -102,9 +99,6 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
     implementation("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
     implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
-
-    // CVE fix
-    implementation("tools.jackson.core:jackson-core:$jackson3CoreVersion")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
