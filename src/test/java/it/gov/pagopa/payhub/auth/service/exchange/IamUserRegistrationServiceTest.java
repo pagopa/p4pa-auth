dd80bafb2265ef27b89e709ac904f7649d35fd14
antocalo <antonio.calo@nttdata.com>
2026-07-30 14:52:45 +0200

    P4ADEV-4896-update-organizationExternalId

--- src/test/java/it/gov/pagopa/payhub/auth/service/exchange/IamUserRegistrationServiceTest.java
+++ src/test/java/it/gov/pagopa/payhub/auth/service/exchange/IamUserRegistrationServiceTest.java
@@ -94,7 +94,7 @@ class IamUserRegistrationServiceTest {
         // Then
         verifyRegisterUserInvocation(userInfo);
         Assertions.assertEquals("ROLES_NOT_FOUND",exception.getCode());
-        Assertions.assertEquals("No roles configured for organizationAccess IamUserOrganizationRolesDTO(organizationIpaCode=ORG2, roles=[], email=null, externalOrganizationId=null)", exception.getMessage());
+        Assertions.assertEquals("No roles configured for organizationAccess IamUserOrganizationRolesDTO(organizationIpaCode=ORG2, externalOrganizationId=null, roles=[], email=null)", exception.getMessage());
     }
 
     private void verifyRegisterUserInvocation(IamUserInfoDTO userInfo) {
